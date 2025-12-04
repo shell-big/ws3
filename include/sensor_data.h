14fa48b2d62237abdda21d50c45cfa477d45e6c3
shell <shellbig.ryo@outlook.jp>
2025-12-04 13:54:26 +0900

    refactor: 🔧 #016 bindings.h をインクルードし、AxisData 構造体の再定義を削除

--- include/sensor_data.h
+++ include/sensor_data.h
@@ -4,18 +4,9 @@
 #include <string>   // std::string を使用するため (現在は直接使用していないが、将来的に使う可能性あり)
 #include <vector>   // ADCデータなどの配列データを扱うために含める (現在は直接使用していない)
 #include <stddef.h> // size_t 型を使用するため
+#include "bindings.h" // AxisData 構造体の定義のためにインクルード
 
 #define SENSOR_BUFFER_SIZE 512 // センサーデータを格納する文字列バッファの推奨サイズ
-
-// 加速度計、ジャイロ、磁力計などの軸データを格納するための構造体
-// この構造体は bindings.h にも存在するが、循環参照を防ぐためにここで再定義する
-// もしくは、bindings.h をインクルードすることで共有することも検討できるが、
-// sensor_data.h がbindings.hに依存しないようにするためにここで定義する。
-struct AxisData {
-    float x;
-    float y;
-    float z;
-};
 
 // 関数のプロトタイプ宣言
 // 関連するすべてのセンサーを読み取り、指定されたバッファに文字列としてフォーマットする
